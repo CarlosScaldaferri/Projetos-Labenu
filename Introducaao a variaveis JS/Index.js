b5402f5d9db7fbece0b98a47f53c9d172f8a3494
CarlosScaldaferri <carlosscaldaferri@gmaail.com>
2022-11-25 12:55:02 -0300

    Primeiro commit

---
+++ Introducaao a variaveis JS/Index.js
@@ -0,0 +1,13 @@
+/* console.log */
+console.log("Olá")
+let nome = prompt("Qual é o seu nome?")
+console.log(nome)
+let idade = prompt("Qual a sua idade?")
+console.log("Olá", nome, "Sua idaade é", idade)
+let temPet
+temPet = confirm("Você tem pet?")
+console.log(temPet)
+const nomeDoPet = prompt("Qual o nome do seu pet?")
+console.log(nomeDoPet)
+
+
